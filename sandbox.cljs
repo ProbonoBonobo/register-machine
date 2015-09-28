8f0f38c2c1d206783783d1f890b86120d5b2e8eb
Kevin Zeidler <kzeidler@gmail.com>
2015-09-28 16:57:49 -0700

    Update sandbox.cljs
    added annotations and gratitude

--- sandbox.cljs
+++ sandbox.cljs
@@ -1,3 +1,10 @@
+; this isn't executed, it's just for reference. special thanks to Chris LaRose (https://github.com/cjlarose/) for
+; this snippet
+; 
+; basic idea: nest the value registers inside the atomic state variable, and maintain a "current-target" pointer
+;             that returns the register currently in focus. this implements something close to the register-transfer
+;             level abstraction typical of control unit architectures
+
 (defn update [val]
    (swap! app-state (fn [current-state]
         (let [current-target (:current-target @app-state)
